a203df393681a81dfcfadd352a97c1774a20d02a
Axellesti <sartori.axelle18@gmail.com>
2024-02-15 12:32:19 +0100

    Challenge méthode POST_DONE

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -12,6 +12,23 @@ const getUsers = (req, res) => {
     });
 };
 
+const postUsers = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 const getUsersById = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -27,4 +44,5 @@ const getUsersById = (req, res) => {
 module.exports = {
   getUsers,
   getUsersById,
+  postUsers,
 };
